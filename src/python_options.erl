c7b61f70b7458aa40f725e7f4b934a763ace333a
wangrui <276361270@qq.com>
2015-11-28 15:16:32 +0800

    modify path

--- src/python_options.erl
+++ src/python_options.erl
@@ -37,14 +37,14 @@
 -author('Dmitry Vasiliev <dima@hlabs.org>').
 
 -export([
-    parse/1
-    ]).
+  parse/1
+]).
 
 -define(TIMEOUT, 15000).
 
 -type option() :: {python, Python :: string()}
-    | {python_path, Path :: string() | [Path :: string()]}
-    | erlport_options:option().
+| {python_path, Path :: string() | [Path :: string()]}
+| erlport_options:option().
 -type options() :: [option()].
 
 -export_type([option/0, options/0]).
@@ -56,135 +56,144 @@
 %% @doc Parse Python options
 %%
 
--spec parse(Options::options()) ->
-    {ok, #python_options{}} | {error, Reason::term()}.
+-spec parse(Options :: options()) ->
+  {ok, #python_options{}} | {error, Reason :: term()}.
 
 parse(Options) when is_list(Options) ->
-    parse(Options, #python_options{}).
+  parse(Options, #python_options{}).
 
 parse([{python, Python} | Tail], Options) ->
-    % Will be checked later
-    parse(Tail, Options#python_options{python=Python});
-parse([{python_path, PythonPath}=Value | Tail], Options) ->
-    case erlport_options:filter_invalid_paths(PythonPath) of
-        {ok, Path} ->
-            % Paths will be checked later
-            parse(Tail, Options#python_options{python_path=Path});
-        {error, Invalid} ->
-            {error, {invalid_option, Value, Invalid}}
-    end;
+  % Will be checked later
+  parse(Tail, Options#python_options{python = Python});
+parse([{python_path, PythonPath} = Value | Tail], Options) ->
+  case erlport_options:filter_invalid_paths(PythonPath) of
+    {ok, Path} ->
+      % Paths will be checked later
+      parse(Tail, Options#python_options{python_path = Path});
+    {error, Invalid} ->
+      {error, {invalid_option, Value, Invalid}}
+  end;
 parse([Option | Tail], Options) ->
-    case erlport_options:parse(Option) of
-        {ok, Name, Value} ->
-            parse(Tail, set_by_name(Name, Value, Options));
-        {error, _}=Error ->
-            Error
-    end;
-parse([], Options=#python_options{env=Env0, python_path=PythonPath0,
-        python=Python, port_options=PortOptions, packet=Packet,
-        cd=Path, use_stdio=UseStdio}) ->
-    PortOptions1 = erlport_options:update_port_options(
-        PortOptions, Path, UseStdio),
-    case get_python(Python) of
-        {ok, PythonFilename, MajVersion} ->
-            case update_python_path(Env0, PythonPath0, MajVersion) of
-                {ok, PythonPath, Env} ->
-                    {ok, Options#python_options{env=Env,
-                        python_path=PythonPath, python=PythonFilename,
-                        port_options=[{env, Env}, {packet, Packet}
-                            | PortOptions1]}};
-                {error, _}=Error ->
-                    Error
-            end;
-        {error, _}=Error ->
-            Error
-    end.
+  case erlport_options:parse(Option) of
+    {ok, Name, Value} ->
+      parse(Tail, set_by_name(Name, Value, Options));
+    {error, _} = Error ->
+      Error
+  end;
+parse([], Options = #python_options{env = Env0, python_path = PythonPath0,
+  python = Python, port_options = PortOptions, packet = Packet,
+  cd = Path, use_stdio = UseStdio}) ->
+  PortOptions1 = erlport_options:update_port_options(
+    PortOptions, Path, UseStdio),
+  case get_python(Python) of
+    {ok, PythonFilename, MajVersion} ->
+      case update_python_path(Env0, PythonPath0, MajVersion) of
+        {ok, PythonPath, Env} ->
+          {ok, Options#python_options{env = Env,
+            python_path = PythonPath, python = PythonFilename,
+            port_options = [{env, Env}, {packet, Packet}
+              | PortOptions1]}};
+        {error, _} = Error ->
+          Error
+      end;
+    {error, _} = Error ->
+      Error
+  end.
 
 %%%
 %%% Utility functions
 %%%
 
 set_by_name(Name, Value, Options) ->
-    case proplists:get_value(Name, ?PYTHON_FIELDS) of
-        N when is_integer(N) andalso N > 1 ->
-            setelement(N, Options, Value)
-    end.
+  case proplists:get_value(Name, ?PYTHON_FIELDS) of
+    N when is_integer(N) andalso N > 1 ->
+      setelement(N, Options, Value)
+  end.
 
 update_python_path(Env0, PythonPath0, MajVersion) ->
-    case code:priv_dir(erlport) of
-        {error, bad_name} ->
-            {error, {not_found, "erlport/priv"}};
-        PrivDir ->
-            PythonDir = lists:concat([python, MajVersion]),
-            ErlPortPath = erlport_options:joinpath(PrivDir, PythonDir),
-            {PathFromSetEnv, Env2} = extract_python_path(Env0, "", []),
-            PathFromEnv = erlport_options:getenv("PYTHONPATH"),
-            PythonPath = erlport_options:join_path([[ErlPortPath], PythonPath0,
-                erlport_options:split_path(PathFromSetEnv),
-                erlport_options:split_path(PathFromEnv)]),
-            Env3 = [{"PYTHONPATH", PythonPath} | Env2],
-            {ok, PythonPath, Env3}
-    end.
+%%   case code:priv_dir(erlport) of
+%%     {error, bad_name} ->
+%%       {error, {not_found, "erlport/priv"}};
+  PrivDir = priv_dir(erlport),
+  PythonDir = lists:concat([python, MajVersion]),
+  ErlPortPath = erlport_options:joinpath(PrivDir, PythonDir),
+  {PathFromSetEnv, Env2} = extract_python_path(Env0, "", []),
+  PathFromEnv = erlport_options:getenv("PYTHONPATH"),
+  PythonPath = erlport_options:join_path([[ErlPortPath], PythonPath0,
+    erlport_options:split_path(PathFromSetEnv),
+    erlport_options:split_path(PathFromEnv)]),
+  Env3 = [{"PYTHONPATH", PythonPath} | Env2],
+  {ok, PythonPath, Env3}.
+%end.
 
 get_python(default) ->
-    case erlport_options:getenv(?PYTHON_VAR_NAME) of
-        "" ->
-            try find_python(?DEFAULT_PYTHON)
-            catch
-                throw:not_found ->
-                    {error, python_not_found}
-            end;
-        Python ->
-            try find_python(Python)
-            catch
-                throw:not_found ->
-                    {error, {invalid_env_var, {?PYTHON_VAR_NAME, Python},
-                        not_found}}
-            end
-    end;
-get_python(Python=[_|_]) ->
-    try find_python(Python)
-    catch
+  case erlport_options:getenv(?PYTHON_VAR_NAME) of
+    "" ->
+      try find_python(?DEFAULT_PYTHON)
+      catch
         throw:not_found ->
-            {error, {invalid_option, {python, Python}, not_found}}
-    end;
+          {error, python_not_found}
+      end;
+    Python ->
+      try find_python(Python)
+      catch
+        throw:not_found ->
+          {error, {invalid_env_var, {?PYTHON_VAR_NAME, Python},
+            not_found}}
+      end
+  end;
+get_python(Python = [_ | _]) ->
+  try find_python(Python)
+  catch
+    throw:not_found ->
+      {error, {invalid_option, {python, Python}, not_found}}
+  end;
 get_python(Python) ->
-    {error, {invalid_option, {python, Python}}}.
+  {error, {invalid_option, {python, Python}}}.
 
 find_python(Python) ->
-    {PythonCommand, Options} = lists:splitwith(fun (C) -> C =/= $ end, Python),
-    case os:find_executable(PythonCommand) of
-        false ->
-            throw(not_found);
-        Filename ->
-            Fullname = erlport_options:absname(Filename),
-            case check_python_version(Fullname) of
-                {ok, {MajVersion, _, _}} ->
-                    {ok, Fullname ++ Options, MajVersion};
-                {error, _}=Error ->
-                    Error
-            end
-    end.
+  {PythonCommand, Options} = lists:splitwith(fun(C) -> C =/= $  end, Python),
+  case os:find_executable(PythonCommand) of
+    false ->
+      throw(not_found);
+    Filename ->
+      Fullname = erlport_options:absname(Filename),
+      case check_python_version(Fullname) of
+        {ok, {MajVersion, _, _}} ->
+          {ok, Fullname ++ Options, MajVersion};
+        {error, _} = Error ->
+          Error
+      end
+  end.
 
 extract_python_path([{"PYTHONPATH", P} | Tail], Path, Env) ->
-    extract_python_path(Tail, [P, erlport_options:pathsep() | Path], Env);
+  extract_python_path(Tail, [P, erlport_options:pathsep() | Path], Env);
 extract_python_path([Item | Tail], Path, Env) ->
-    extract_python_path(Tail, Path, [Item | Env]);
+  extract_python_path(Tail, Path, [Item | Env]);
 extract_python_path([], Path, Env) ->
-    {lists:append(lists:reverse(Path)), lists:reverse(Env)}.
+  {lists:append(lists:reverse(Path)), lists:reverse(Env)}.
 
 check_python_version(Python) ->
-    Out = erlport_options:get_version(Python ++ " -V"),
-    case re:run(Out, "^Python ([0-9]+)\\.([0-9]+)\\.([0-9]+)",
-            [{capture, all_but_first, list}]) of
-        {match, StrVersion} ->
-            Version = list_to_tuple([list_to_integer(N) || N <- StrVersion]),
-            if
-                Version >= {2, 5, 0} ->
-                    {ok, Version};
-                true ->
-                    {error, {unsupported_python_version, Out}}
-            end;
-        nomatch ->
-            {error, {invalid_python, Python}}
-    end.
+  Out = erlport_options:get_version(Python ++ " -V"),
+  case re:run(Out, "^Python ([0-9]+)\\.([0-9]+)\\.([0-9]+)",
+    [{capture, all_but_first, list}]) of
+    {match, StrVersion} ->
+      Version = list_to_tuple([list_to_integer(N) || N <- StrVersion]),
+      if
+        Version >= {2, 5, 0} ->
+          {ok, Version};
+        true ->
+          {error, {unsupported_python_version, Out}}
+      end;
+    nomatch ->
+      {error, {invalid_python, Python}}
+  end.
+
+
+priv_dir(App) ->
+  case code:priv_dir(App) of
+    {error, bad_name} ->
+      io:format("Couldn't find priv dir for port_server, using ./priv ~n"),
+      "./priv";
+    PrivDir -> filename:absname(PrivDir)
+  end.
--- src/ruby_options.erl
+++ src/ruby_options.erl
@@ -37,13 +37,13 @@
 -author('Dmitry Vasiliev <dima@hlabs.org>').
 
 -export([
-    parse/1
-    ]).
+  parse/1
+]).
 
 
 -type option() :: {ruby, Ruby :: string()}
-    | {ruby_lib, Path :: string() | [Path :: string()]}
-    | erlport_options:option().
+| {ruby_lib, Path :: string() | [Path :: string()]}
+| erlport_options:option().
 -type options() :: [option()].
 
 -export_type([option/0, options/0]).
@@ -55,138 +55,146 @@
 %% @doc Parse Ruby options
 %%
 
--spec parse(Options::options()) ->
-    {ok, #ruby_options{}} | {error, Reason::term()}.
+-spec parse(Options :: options()) ->
+  {ok, #ruby_options{}} | {error, Reason :: term()}.
 
 parse(Options) when is_list(Options) ->
-    parse(Options, #ruby_options{}).
+  parse(Options, #ruby_options{}).
 
 parse([{ruby, Ruby} | Tail], Options) ->
-    % Will be checked later
-    parse(Tail, Options#ruby_options{ruby=Ruby});
-parse([{ruby_lib, RubyLib}=Value | Tail], Options) ->
-    case erlport_options:filter_invalid_paths(RubyLib) of
-        {ok, Path} ->
-            % Paths will be checked later
-            parse(Tail, Options#ruby_options{ruby_lib=Path});
-        {error, Invalid} ->
-            {error, {invalid_option, Value, Invalid}}
-    end;
+  % Will be checked later
+  parse(Tail, Options#ruby_options{ruby = Ruby});
+parse([{ruby_lib, RubyLib} = Value | Tail], Options) ->
+  case erlport_options:filter_invalid_paths(RubyLib) of
+    {ok, Path} ->
+      % Paths will be checked later
+      parse(Tail, Options#ruby_options{ruby_lib = Path});
+    {error, Invalid} ->
+      {error, {invalid_option, Value, Invalid}}
+  end;
 parse([Option | Tail], Options) ->
-    case erlport_options:parse(Option) of
-        {ok, Name, Value} ->
-            parse(Tail, set_by_name(Name, Value, Options));
-        {error, _}=Error ->
-            Error
-    end;
-parse([], Options=#ruby_options{env=Env0, ruby_lib=RubyLib0,
-        ruby=Ruby, port_options=PortOptions, packet=Packet,
-        cd=Path, use_stdio=UseStdio}) ->
-    PortOptions1 = erlport_options:update_port_options(
-        PortOptions, Path, UseStdio),
-    case get_ruby(Ruby) of
-        {ok, RubyFilename, MajVersion} ->
-            case update_ruby_lib(Env0, RubyLib0, MajVersion) of
-                {ok, RubyPath, Env} ->
-                    {ok, Options#ruby_options{env=Env,
-                        ruby_lib=RubyPath, ruby=RubyFilename,
-                        port_options=[{env, Env}, {packet, Packet}
-                            | PortOptions1]}};
-                {error, _}=Error ->
-                    Error
-            end;
-        {error, _}=Error ->
-            Error
-    end.
+  case erlport_options:parse(Option) of
+    {ok, Name, Value} ->
+      parse(Tail, set_by_name(Name, Value, Options));
+    {error, _} = Error ->
+      Error
+  end;
+parse([], Options = #ruby_options{env = Env0, ruby_lib = RubyLib0,
+  ruby = Ruby, port_options = PortOptions, packet = Packet,
+  cd = Path, use_stdio = UseStdio}) ->
+  PortOptions1 = erlport_options:update_port_options(
+    PortOptions, Path, UseStdio),
+  case get_ruby(Ruby) of
+    {ok, RubyFilename, MajVersion} ->
+      case update_ruby_lib(Env0, RubyLib0, MajVersion) of
+        {ok, RubyPath, Env} ->
+          {ok, Options#ruby_options{env = Env,
+            ruby_lib = RubyPath, ruby = RubyFilename,
+            port_options = [{env, Env}, {packet, Packet}
+              | PortOptions1]}};
+        {error, _} = Error ->
+          Error
+      end;
+    {error, _} = Error ->
+      Error
+  end.
 
 %%%
 %%% Utility functions
 %%%
 
 set_by_name(Name, Value, Options) ->
-    case proplists:get_value(Name, ?RUBY_FIELDS) of
-        N when is_integer(N) andalso N > 1 ->
-            setelement(N, Options, Value)
-    end.
+  case proplists:get_value(Name, ?RUBY_FIELDS) of
+    N when is_integer(N) andalso N > 1 ->
+      setelement(N, Options, Value)
+  end.
 
 update_ruby_lib(Env0, RubyPath0, MajVersion) ->
-    case code:priv_dir(erlport) of
-        {error, bad_name} ->
-            {error, {not_found, "erlport/priv"}};
-        PrivDir ->
-            RubyDir = lists:concat([ruby, MajVersion]),
-            ErlPortPath = erlport_options:joinpath(PrivDir, RubyDir),
-            {PathFromSetEnv, Env2} = extract_ruby_lib(Env0, "", []),
-            PathFromEnv = erlport_options:getenv("RUBYLIB"),
-            RubyPath = erlport_options:join_path([[ErlPortPath], RubyPath0,
-                erlport_options:split_path(PathFromSetEnv),
-                erlport_options:split_path(PathFromEnv)]),
-            Env3 = [{"RUBYLIB", RubyPath} | Env2],
-            {ok, RubyPath, Env3}
-    end.
+%%     case code:priv_dir(erlport) of
+%%         {error, bad_name} ->
+%%             {error, {not_found, "erlport/priv"}};
+  PrivDir = priv_dir(erlport),
+  RubyDir = lists:concat([ruby, MajVersion]),
+  ErlPortPath = erlport_options:joinpath(PrivDir, RubyDir),
+  {PathFromSetEnv, Env2} = extract_ruby_lib(Env0, "", []),
+  PathFromEnv = erlport_options:getenv("RUBYLIB"),
+  RubyPath = erlport_options:join_path([[ErlPortPath], RubyPath0,
+    erlport_options:split_path(PathFromSetEnv),
+    erlport_options:split_path(PathFromEnv)]),
+  Env3 = [{"RUBYLIB", RubyPath} | Env2],
+  {ok, RubyPath, Env3}.
+%% end.
 
 get_ruby(default) ->
-    case erlport_options:getenv(?RUBY_VAR_NAME) of
-        "" ->
-            try find_ruby(?DEFAULT_RUBY)
-            catch
-                throw:not_found ->
-                    {error, ruby_not_found}
-            end;
-        Ruby ->
-            try find_ruby(Ruby)
-            catch
-                throw:not_found ->
-                    {error, {invalid_env_var, {?RUBY_VAR_NAME, Ruby},
-                        not_found}}
-            end
-    end;
-get_ruby(Ruby=[_|_]) ->
-    try find_ruby(Ruby)
-    catch
+  case erlport_options:getenv(?RUBY_VAR_NAME) of
+    "" ->
+      try find_ruby(?DEFAULT_RUBY)
+      catch
         throw:not_found ->
-            {error, {invalid_option, {ruby, Ruby}, not_found}}
-    end;
+          {error, ruby_not_found}
+      end;
+    Ruby ->
+      try find_ruby(Ruby)
+      catch
+        throw:not_found ->
+          {error, {invalid_env_var, {?RUBY_VAR_NAME, Ruby},
+            not_found}}
+      end
+  end;
+get_ruby(Ruby = [_ | _]) ->
+  try find_ruby(Ruby)
+  catch
+    throw:not_found ->
+      {error, {invalid_option, {ruby, Ruby}, not_found}}
+  end;
 get_ruby(Ruby) ->
-    {error, {invalid_option, {ruby, Ruby}}}.
+  {error, {invalid_option, {ruby, Ruby}}}.
 
 find_ruby(Ruby) ->
-    {RubyCommand, Options} = lists:splitwith(fun (C) -> C =/= $ end, Ruby),
-    case os:find_executable(RubyCommand) of
-        false ->
-            throw(not_found);
-        Filename ->
-            Fullname = erlport_options:absname(Filename),
-            case check_ruby_version(Fullname) of
-                {ok, {MajVersion, MinVersion, _}} ->
-                    Version = lists:concat([MajVersion, ".", MinVersion]),
-                    {ok, Fullname ++ Options, Version};
-                {error, _}=Error ->
-                    Error
-            end
-    end.
+  {RubyCommand, Options} = lists:splitwith(fun(C) -> C =/= $  end, Ruby),
+  case os:find_executable(RubyCommand) of
+    false ->
+      throw(not_found);
+    Filename ->
+      Fullname = erlport_options:absname(Filename),
+      case check_ruby_version(Fullname) of
+        {ok, {MajVersion, MinVersion, _}} ->
+          Version = lists:concat([MajVersion, ".", MinVersion]),
+          {ok, Fullname ++ Options, Version};
+        {error, _} = Error ->
+          Error
+      end
+  end.
 
 extract_ruby_lib([{"RUBYLIB", P} | Tail], Path, Env) ->
-    extract_ruby_lib(Tail, [P, erlport_options:pathsep() | Path], Env);
+  extract_ruby_lib(Tail, [P, erlport_options:pathsep() | Path], Env);
 extract_ruby_lib([Item | Tail], Path, Env) ->
-    extract_ruby_lib(Tail, Path, [Item | Env]);
+  extract_ruby_lib(Tail, Path, [Item | Env]);
 extract_ruby_lib([], Path, Env) ->
-    {lists:append(lists:reverse(Path)), lists:reverse(Env)}.
+  {lists:append(lists:reverse(Path)), lists:reverse(Env)}.
 
 check_ruby_version(Ruby) ->
-    Out = erlport_options:get_version(Ruby ++ " -v"),
-    case re:run(Out, "^ruby ([0-9]+)\\.([0-9]+)\\.([0-9]+)",
-            [{capture, all_but_first, list}]) of
-        {match, StrVersion} ->
-            Version = list_to_tuple([list_to_integer(N) || N <- StrVersion]),
-            if
-                Version >= {1, 8, 6} andalso Version < {2, 0, 0} ->
-                    {ok, Version};
-                Version >= {2, 0, 0} ->
-                    {ok, {1, 9, 0}};
-                true ->
-                    {error, {unsupported_ruby_version, Out}}
-            end;
-        nomatch ->
-            {error, {invalid_ruby, Ruby}}
-    end.
+  Out = erlport_options:get_version(Ruby ++ " -v"),
+  case re:run(Out, "^ruby ([0-9]+)\\.([0-9]+)\\.([0-9]+)",
+    [{capture, all_but_first, list}]) of
+    {match, StrVersion} ->
+      Version = list_to_tuple([list_to_integer(N) || N <- StrVersion]),
+      if
+        Version >= {1, 8, 6} andalso Version < {2, 0, 0} ->
+          {ok, Version};
+        Version >= {2, 0, 0} ->
+          {ok, {1, 9, 0}};
+        true ->
+          {error, {unsupported_ruby_version, Out}}
+      end;
+    nomatch ->
+      {error, {invalid_ruby, Ruby}}
+  end.
+
+priv_dir(App) ->
+  case code:priv_dir(App) of
+    {error, bad_name} ->
+      io:format("Couldn't find priv dir for port_server, using ./priv ~n"),
+      "./priv";
+    PrivDir -> filename:absname(PrivDir)
+  end.
